93fa976f6a84e6dae54f4ce2d7292c72918185d5
Jakub Świder <jakub.swider999@gmail.com>
2021-11-03 19:21:45 +0100

    Solution to problem 03

--- solutions/1-10.ex
+++ solutions/1-10.ex
@@ -2,17 +2,23 @@
 
 defmodule P0 do
 
-# Problem 01
+# Problem 01 - Write a function last : 'a list -> 'a option that returns the last element of a list.
 
     def last([]), do: nil
     def last([a]), do: a
     def last([ _ | tail ]), do: last(tail)
 
-# Problem 02
+# Problem 02 - Find the last but one (last and penultimate) elements of a list.
 
     def last_two([]), do: nil
     def last_two([_]), do: nil
     def last_two([a, b]), do: [a, b]
     def last_two( [ _ | tail ] ), do: last_two(tail)
+
+# Problem 03 - Find the K'th element of a list.
+
+    def at(_, []), do: nil
+    def at(1, [ head | _ ]), do: head
+    def at(i, [ _ | tail ]), do: at(i - 1, tail)
 
 end
